84fd06c4017a89cba5154062073f5b755577b071
Chris Roberts <cpr420@gmail.com>
2023-03-25 23:30:36 -0600

    add video thumbnail test command

---
+++ TestCommands/create_video_thumbnails.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if (($# == 0)); then
+	alert --stop "Nothing selected"
+	exit
+fi
+
+if ! type -p ffmpegthumbnailer >/dev/null;then
+	alert --stop "'ffmpegthumbnailer' command not found.  Pleast install the ffmpegthumbnailer package."
+fi
+
+if ! type -p gm >/dev/null;then
+	alert --stop "'gm' command not found.  Pleast install the GraphicsMagick package."
+fi
+
+function nail_it {
+	videoFile="$@"
+	thumbnailFile="${videoFile%.*}Thumbnail_$$.png"
+
+	ffmpegthumbnailer -i "$videoFile" -o "$thumbnailFile"
+
+	if (($? != 0));then
+		alert --stop "Error generating thumbnail for $videoFile"
+		rm -f "$thumbnailFile"
+		return
+	fi
+
+	## clear out any existing thumbnail data
+	rmattr -p "Media:Thumbnail*" "$videoFile" 2>/dev/null
+
+	## make Tracker happy with a square image
+	gm convert "$thumbnailFile" -background none -gravity center -extent 128x128 "$thumbnailFile"
+
+	if (($? == 0));then
+		addattr -t time Media:Thumbnail:CreationTime `date +%s` "$videoFile"
+		addattr -f "$thumbnailFile" -t raw Media:Thumbnail "$videoFile"
+	else
+		alert --stop "Error resizing thumbnail for $videoFile"
+	fi
+
+	rm -f "$thumbnailFile"
+}
+
+
+for ((i = 1; i <= $#; i++)); do
+	nail_it ${!i}
+done
